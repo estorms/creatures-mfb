d9bdf8440900a32f8d5df39f41d65fa466987be5
Elizabeth Sanger <libsang@gmail.com>
2016-08-21 16:43:06 -0500

    images outputting to DOM

--- scripts.js
+++ scripts.js
@@ -8,13 +8,20 @@ let imagesArray = [];
 function useImages(images) {
 // var x = images.url;
     for (var i in images) {
-        console.log('images working')
+        console.log('images working');
         imagesArray.push(images[i]);
     }
+
     imagesArray.forEach(function(img){
-        console.log(img.url)
-        $('.images-container').attr()
+        var pic = $('<img />').attr('src', img.url).attr('title', name)
+
+      $('.images-container').append(pic)
     })
+
+    $('img').first().addClass('active')
+    $('img').second().addClass('hidden')
+
+
 };
 
 function loadImages(useImages) {

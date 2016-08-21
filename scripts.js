e2e987079d0bdb6dff99bf04f70b19d828b2182c
Elizabeth Sanger <libsang@gmail.com>
2016-08-21 16:13:39 -0500

    loading data from json and beginning to populate page

--- scripts.js
+++ scripts.js
@@ -1,1 +1,32 @@
 console.log('scripts working');
+
+$(document).ready( () => {
+
+let images;
+let imagesArray = [];
+
+function useImages(images) {
+// var x = images.url;
+    for (var i in images) {
+        console.log('images working')
+        imagesArray.push(images[i]);
+    }
+    imagesArray.forEach(function(img){
+        console.log(img.url)
+        $('.images-container').attr()
+    })
+};
+
+function loadImages(useImages) {
+$.ajax({
+  url: "data.json"
+}).then(function(data) {
+  images = data.images[0];
+  useImages(images);
+});
+};
+
+loadImages(useImages);
+
+
+})

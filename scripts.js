ff5ff1add4754368ccf3c81addf2f42b95e835c0
Elizabeth Sanger <libsang@gmail.com>
2016-08-22 20:45:40 -0500

    basic functionality working, still has bugs

--- scripts.js
+++ scripts.js
@@ -13,14 +13,37 @@ function useImages(images) {
     }
 
     imagesArray.forEach(function(img){
-        var pic = $('<img />').attr('src', img.url).attr('title', name)
+        var pic = $('<img />').attr('src', img.url).attr('title', name).addClass('.pic')
 
       $('.images-container').append(pic)
+
     })
 
     $('img').first().addClass('active')
-    $('img').second().addClass('hidden')
 
+$('.next').click(function () {       //problems using arrow functions here!
+    var activeImage = $('img.active')
+    console.log(activeImage)
+    console.log(activeImage.next())
+    var nextImage //do you need to use variables? looks like it
+      = activeImage.next().length  //figure out what the length is all about is
+      ? activeImage.next()
+      : $('img').first() //refresh on ternaries
+
+    activeImage.removeClass('active')
+    nextImage.addClass('active')
+  })
+
+  $('.prev').click(function () {
+    var activeImage = $('img.active')
+    var nextImage
+      = activeImage.prev().length
+      ? activeImage.prev()
+      : $('img').last()
+
+    activeImage.removeClass('active')
+    nextImage.addClass('active')
+  })
 
 };
 

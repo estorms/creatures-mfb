33c87f1afe0fc6c5e5687f96ad0006dd34baff97
Elizabeth Sanger <libsang@gmail.com>
2016-08-24 19:25:04 -0500

    turned into promise, just one hiccough left to resolve

--- scripts.js
+++ scripts.js
@@ -1,21 +1,21 @@
-$(document).ready( () => {
+
 
 let images;
 let imagesArray = [];
 
-function useImages(images) {
-// var x = images.url;
-    for (var i in images) {
-        console.log('images working');
-        imagesArray.push(images[i]);
-    }
+// function useImages(images) {
+// // var x = images.url;
+//     for (var i in images) {
+//         console.log('images working');
+//         imagesArray.push(images[i]);
+//     }
 
-    imagesArray.forEach(function(img){
-        var pic = $('<img />').attr('src', img.url).attr('title', name).addClass('.pic')
+//     imagesArray.forEach(function(img){
+//         var pic = $('<img />').attr('src', img.url).attr('title', name).addClass('.pic')
 
-      $('.images-container').append(pic);
+//       $('.images-container').append(pic);
 
-    });
+//     });
 
     $('img').first().addClass('active');
 
@@ -33,18 +33,63 @@ $('.next').click(function () {       //problems using arrow functions here!
     nextImage.addClass('active');
   });
 
-};
 
-function loadImages(useImages) {
-$.ajax({
-  url: "data.json"
-}).then(function(data) {
-  images = data.images[0];
-  useImages(images);
-});
-};
 
-loadImages(useImages);
+// function loadImages(useImages) {
+// $.ajax({
+//   url: "data.json"
+// }).then(function(data) {
+//   images = data.images[0];
+//   useImages(images);
+// });
+// };
+
+// loadImages(useImages);
+
+function loadImages(images) {
+  return new Promise(function(resolve, reject) {
+    $.ajax({
+      url: 'data.json'
+    }).done(function(data){
+      resolve(data);
+      console.log('data', data)
+    }).fail(function(error){
+      reject(console.log('error', error))
+    })
+  })
+}
+
+loadImages(images)
+.then(function (images) {
+// console.log(images)
+    images = images;
+    for (var i in images) {
+        console.log('images working');
+        imagesArray.push(images[i]);
+        console.log(imagesArray)
+    }
+
+    imagesArray.forEach(function(img){
+        var pic = $('<img />').attr('src', img.url).attr('title', name).addClass('.pic')
+
+      $('.images-container').append(pic);
+
 
 
 })
+})
+
+//VERY CLOSE, JUST RUNNING INTO THE SAME INDEXING ERROR (IMGAGES[0]) AS BEFORE
+
+    // var x = images.url;
+    // for (var i in images) {
+    //     console.log('images working');
+    //     imagesArray.push(images[i]);
+    // }
+
+    // imagesArray.forEach(function(img){
+    //     var pic = $('<img />').attr('src', img.url).attr('title', name).addClass('.pic')
+
+    //   $('.images-container').append(pic);
+
+
